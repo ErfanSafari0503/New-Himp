2fb5235cd6b76409d63131bead1c4c76910f7e0d
Erfan Safari <erfansafari0503@gmail.com>
2025-05-30 16:22:38 +0330

    feat(button): add support for routing and customizable styles

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,7 @@
-function Button({ children, type, func }) {
-  //   const baseStyle = '';
+import { Link } from 'react-router-dom';
+
+function Button({ children, type, to, func, color, shadow, linkStyle }) {
+  const baseStyle = `rounded-2xl ${color} px-6 py-4 ${shadow}`;
   const style = {
     headerMenuIcon: 'transition-all duration-300',
   };
@@ -10,6 +12,20 @@ function Button({ children, type, func }) {
         {children}
       </button>
     );
+
+  return (
+    <button className={baseStyle}>
+      <Link
+        to={to}
+        className={
+          linkStyle ||
+          'font-DM-Sans text-base font-semibold tracking-wide text-gray-800'
+        }
+      >
+        {children}
+      </Link>
+    </button>
+  );
 }
 
 export default Button;
